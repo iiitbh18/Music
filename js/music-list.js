46d2272abbc8ef2d4b3b5ba53f148b2e1840283f
Bhaskar Kumar <bhaskarkumar654321@gmail.com>
2022-05-11 17:35:25 +0530

    first commit

---
+++ js/music-list.js
@@ -0,0 +1,151 @@
+// To add more song, just copy the following code and paste inside the array
+
+//   {
+//     name: "Here is the music name",
+//     artist: "Here is the artist name",
+//     img: "image name here - remember img must be in .jpg formate and it's inside the images folder of this project folder",
+//     src: "music name here - remember img must be in .mp3 formate and it's inside the songs folder of this project folder"
+//   }
+
+//paste it inside the array as more as you want music then you don't need to do any other thing
+
+let allMusic = [
+    {
+      name: "All is well",
+      artist: "Sonu nigam, Shaan",
+      img: "music-1",
+      src: "music-1"
+    },
+    {
+      name: "Behti hawa sa tha wo",
+      artist: "Shaan,Shantanu",
+      img: "music-2",
+      src: "music-2"
+    },
+
+    {
+        name: "Give me some sun shine",
+        artist: "Suraj Jagan, Sharman Joshi",
+        img: "music-3",
+        src: "music-3"
+      },
+
+
+
+
+
+
+    {
+      name: "Champion - D.J Bravo",
+      artist: "Dwayne Bravo",
+      img: "music-4",
+      src: "music-4"
+    },
+    {
+      name: " Zindagi kuch to bta ",
+      artist: "Jubin Natiyal, Pritam",
+      img: "music-5",
+      src: "music-5"
+    },
+    {
+      name: "Let me love you",
+      artist: "Justin Bieber",
+      img: "music-6",
+      src: "music-6"
+    },
+    {
+      name: "Love me Like u do",
+      artist: "Ellie Goulding",
+      img: "music-7",
+      src: "music-7"
+    },
+    {
+      name: "Maston Ka jhund",
+      artist: "Shankar-Ehsaan-Loy",
+      img: "music-8",
+      src: "music-8"
+    },
+    {
+      name: "Pasoori",
+      artist: "Shae gill, Ali sethi",
+      img: "music-9",
+      src: "music-9"
+    },
+    {
+      name: "Rockabye",
+      artist: "Clean Bandit, Sean Paul, Annie Marrrie",
+      img: "music-10",
+      src: "music-10"
+    },
+    
+    {
+      name: "Kun Faya Kun' ",
+      artist: "A.R rahman, Javed Ali, Mohit Chauhan",
+      img: "music-11",
+      src: "music-11"
+    },
+    {
+      name: "Nadan Parindey",
+      artist: "A. R Rahman",
+      img: "music-12",
+      src: "music-12"
+    },
+    {
+      name: "Sajde  ",
+      artist: " Arijit Singh, gulzar, Shankar-Ehsan-Loy, Nihira Joshi Deshpande",
+      img: "music-13",
+      src: "music-13"
+    },
+    {
+      name: "Shape Of You",
+      artist: "Ed Sheeran",
+      img: "music-14",
+      src: "music-14"
+    },
+    {
+      name: "Sia Cheap Thrills",
+      artist: "Sia",
+      img: "music-15",
+      src: "music-15"
+    },
+    {
+      name: "Sia Unstoabble",
+      artist: "Sia",
+      img: "music-16",
+      src: "music-16"
+    },
+    {
+      name: "Let me love you",
+      artist: "Justin Bieber",
+      img: "music-17",
+      src: "music-17"
+    },
+    {
+      name: "Ukulele....",
+      artist: "Not Known",
+      img: "ukulele",
+      src: "ukulele"
+    },
+    {
+      name: "Summer.... ",
+      artist: "Loku",
+      img: "summer",
+      src: "summer"
+    },
+    {
+      name: "Hey ",
+      artist: "Not Known",
+      img: "hey",
+      src: "hey"
+    },
+  
+  
+    // like this paste it and remember to give comma after ending of this bracket }
+    // {
+    //   name: "Here is the music name",
+    //   artist: "Here is the artist name",
+    //   img: "image name here - remember img must be in .jpg formate and it's inside the images folder of this project folder",
+    //   src: "music name here - remember img must be in .mp3 formate and it's inside the songs folder of this project folder"
+    // }
+  ];
+    
